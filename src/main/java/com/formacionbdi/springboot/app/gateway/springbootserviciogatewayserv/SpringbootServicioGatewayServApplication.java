fcfde776a102c8c3c4fcfc62bd2579641691611a
Kenyi De la cruz <kenyi_dlcp_93@outlook.com>
2022-07-05 18:52:31 -0500

    first commit

---
+++ src/main/java/com/formacionbdi/springboot/app/gateway/springbootserviciogatewayserv/SpringbootServicioGatewayServApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.gateway.springbootserviciogatewayserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioGatewayServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioGatewayServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formacionbdi/springboot/app/gateway/springbootserviciogatewayserv/SpringbootServicioGatewayServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.gateway.springbootserviciogatewayserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioGatewayServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
